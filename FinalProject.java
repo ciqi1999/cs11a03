27e25513b189cb4aa767de7bf301c571705d24b1
Gracenut <Grace@Gracenut.local>
2018-11-07 17:49:32 -0500

    add file
    file

--- FinalProject.java
+++ FinalProject.java
@@ -1,8 +1,9 @@
-import java.util.*；
+import java.util.*;
 
 public class FinalProject {
   public static void main(String[] args){
-  	Scanner input = new Scanner(new File(dictionary.txt));
+  	File f = new File("dictionary.txt");
+  	Scanner input = new Scanner(f);
   	String word = input.nextLine();
   	System.out.println(word);
     System.out.println("Welcome to our Word Guessing Game!");
