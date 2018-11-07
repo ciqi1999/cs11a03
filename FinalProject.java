4d33a8c871fc8003a6b103af7d77c4d92739a01e
Gracenut <Grace@Gracenut.local>
2018-11-07 17:42:36 -0500

    read file
    sample code

--- FinalProject.java
+++ FinalProject.java
@@ -3,6 +3,7 @@ import java.util.*；
 public class FinalProject {
   public static void main(String[] args){
   	Scanner input = new Scanner(new File(dictionary.txt));
+  	String word = input.nextLine();
     System.out.println("Welcome to our Word Guessing Game!");
     
   }
