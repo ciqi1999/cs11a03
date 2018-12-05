20ac3851bcb6cdc307b945570660f231893d1976
Sindy Sura <sindysura@brandeis.edu>
2018-12-05 00:39:54 -0500

    Updated JavaDoc Comments

--- FinalProject.java
+++ FinalProject.java
@@ -13,16 +13,13 @@ public class FinalProject {
     System.out.println("Let's start the game.");
     System.out.println("Please enter your guess in lowercase.");
     while(playGame){
-      //input Grace's method
       char[] letters = createLetters(words, index);
       char[] blanks = createBlanks(words, index);
       printBlanks(blanks);
       System.out.println();
 
-      //input Sindy's method
       userGuesses(letters, blanks, words[index]);
 
-      //input Cindy's method
       System.out.println("You win! Do you want to play again? Please enter \"yes\" or \"no\":");
       if (userInput.nextLine().equals("no")){
         System.out.println("Goodbye");
@@ -32,13 +29,12 @@ public class FinalProject {
       }
     }
   }
-  //Grace's Code
 
   /**
-  reads words from the file and puts them into an array
-  @param file: the file containing the list of mystery words
-  @return array words[] which contains all the mystery words
-  */
+   * reads words from the file and puts them into an array
+   * @param file: the file containing the list of mystery words
+   * @return array words[] which contains all the mystery words
+   */
   public static String[] getDictionary(String file) throws FileNotFoundException{
     Scanner input1 = new Scanner(new File(file));
     Scanner input2 = new Scanner(new File(file));
@@ -53,14 +49,13 @@ public class FinalProject {
     for(int i = 0; i < count; i++){
       words[i] = input2.next();
     }
-    //return the array.
     return words;
   }
 
   /**
-  shuffles the values within the array of mystery words
-  @param words: array of mystery words
-  */
+   * shuffles the values within the array of mystery words
+   * @param words: array of mystery words
+   */
   public static void shuffle(String[] words){
     Random r = new Random();
     for(int i = 0; i < words.length; i++){
@@ -72,11 +67,11 @@ public class FinalProject {
   }
 
   /**
-  puts the letters (of the word at array[index]) into an array of chars
-  @param words: array of mystery words
-  @param index: index of array
-  @return array letters[], containing the mystery word's letters
-  */
+   * puts the letters (of the word at array[index]) into an array of chars
+   * @param words: array of mystery words
+   * @param index: index of array
+   * @return array letters[], containing the mystery word's letters
+   */
   public static char[] createLetters(String[] words, int index){
     int l = words[index].length();
     char[] letters = new char[l];
@@ -87,11 +82,11 @@ public class FinalProject {
   }
 
   /**
-  creates an array of underscores, with the number of underscores equal to the number of letters in the mystery word
-  @param words: array of mystery words
-  @param index: index of array
-  @return array blanks[], containing the underscores
-  */
+   * creates an array of underscores, with the number of underscores equal to the number of letters in the mystery word
+   * @param words: array of mystery words
+   * @param index: index of array
+   * @return array blanks[], containing the underscores
+   */
   public static char[] createBlanks(String[] words, int index){
     int l = words[index].length();
     char[] blanks = new char[l];
@@ -102,9 +97,9 @@ public class FinalProject {
   }
 
   /**
-  Prints the mystery word's letters into the array of underscores
-  @param blanks: array of underscores
-  */
+   * prints the mystery word's letters into the array of underscores
+   * @param blanks: array of underscores
+   */
   public static void printBlanks(char[] blanks){
     int l = blanks.length;
     for(int i = 0; i < l; i++){
@@ -113,53 +108,50 @@ public class FinalProject {
     System.out.println();
   }
 
-  //Sindy's Code
+  /**
+   * prompts and checks if the user's guess is in the mystery word
+   * @param letterArray: array of mystery word's letters
+   * @param blankArray: array of underscores
+   * @param word: mystery word being guessed
+   */
   public static void userGuesses(char[] letterArray, char[] blankArray, String word){
     Scanner input = new Scanner (System.in);
     int preCount = blankArray.length; //Program uses this to penalize the user for incorrect guesses
     int postCount= preCount;
-    int blank; // Program uses this to see if more guesses can be made
+    int blank; //Program uses this to see if more guesses can be made
     int lives = 10;
 
-    //Prompt user to enter a letter
     do {
       blank= 0;
       preCount= postCount;
       System.out.print("Guess a letter: ");
       char letter = input.next().charAt(0);
 
-      //Program checks if the word has that letter
-      for (int index = 0; index < blankArray.length; index++){
-        //If Yes,
-        if (letterArray[index]==letter){
-          //Prints that letter onto the place where the underscore has the location
-          blankArray[index] = letter;
+      for (int index = 0; index < blankArray.length; index++){ //Program checks if the word has that letter
+        if (letterArray[index]==letter){  //If Yes,
+          blankArray[index] = letter;     //print that letter in place of the underscore
           postCount--;
         } else if (blankArray[index]=='_'){
           blank++;
         }
       }
-
-      //If No,
-      lives= wrongGuess(word, letter, lives, preCount, postCount);
+      lives= wrongGuess(word, letter, lives, preCount, postCount); //If No, take away a life
       printBlanks(blankArray);
       System.out.println();
-    //Program checks if there are any more blank spaces. If yes, prompt user for another guess.
-    } while (blank!=0);
+    } while (blank!=0); //If there are more blank spaces, prompt user for another guess.
   }
 
   /**
-    returns the numbers of lives the user has after making an incorrect guess
-    @param word: mystery word being guessed
-    @param letter: the letter the user enters as their guess
-    @param lives: the number of lives the user has
-    @param preCount: number of blank spaces leftover before the user enters their guess
-    @param postCount: number of blank spaces leftover after the user enters their guess
-    @return the number of lives leftover
-    */
+   * returns the numbers of lives the user has after making an incorrect guess
+   * @param word: mystery word being guessed
+   * @param letter: the letter the user enters as their guess
+   * @param lives: the number of lives the user has
+   * @param preCount: number of blank spaces leftover before the user enters their guess
+   * @param postCount: number of blank spaces leftover after the user enters their guess
+   * @return the number of lives leftover
+   */
   public static int wrongGuess(String word, char letter, int lives, int preCount, int postCount){
     if (postCount==preCount){
-      //Takes point away
       lives--;
       if (lives!=0){
         System.out.printf("Nope, no \"%c\".%n", letter);
@@ -167,7 +159,6 @@ public class FinalProject {
         System.out.printf("Nope, no \"%c\". %n", letter);
         System.out.printf("The word was \"%s\".%n", word);
         System.out.printf("Game over.%n");
-
         System.exit(1);
       }
     }
